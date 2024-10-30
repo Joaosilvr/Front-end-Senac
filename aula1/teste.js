1106a1019863bcabb58a96742cafeb19915cd8f0
joaosilvr <joaovitor.dasilvaramos@gmail.com>
2024-10-29 21:42:01 -0300

    enviando exercicios

---
+++ aula1/teste.js
@@ -0,0 +1,3 @@
+// 3-     Alfabeto sem números primos: Você recebe um array contento todas as letras do alfabeto “alphabet”. O seu objetivo é criar uma lista que contém apenas os elementos que se encontram em índices não primos, considerando o primeiro índice da lista como 1 e não como 0. example (considering the top 3 not prime numbers): Input: alphabet: {a, b, c, d, e, f, g, ...} Output: result: {a, d, f, …}
+
+
